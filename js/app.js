8e1b2cbffcda2d83b6c05f44c3c04b3c87d7883b
pdybel <pablodybel@hotmail.com>
2023-02-14 15:06:14 -0300

    commit filtros llenar objeto

--- js/app.js
+++ js/app.js
@@ -1,11 +1,35 @@
 //variables
-
-const resultado = document.querySelector("#resultado");
+const marca = document.querySelector("#marca");
 const year = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+
+
+
+// contenedor para los resultados
+const resultado = document.querySelector("#resultado");
+
 
 const max = new Date().getFullYear();
 const min = max - 10;
 
+//genero un objeto con la busqueda
+
+const datosBusqueda = {
+
+    marca: " ",
+    year: " ",
+    minimo: " ",
+    maximo: " ",
+    puertas: " ",
+    transmision: " ",
+    color: " ",
+
+}
 
 //eventos
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,7 +37,52 @@ document.addEventListener("DOMContentLoaded", () => {
 
     llenarSelect();  // Llena las opciones de años
 })    
+
+// Event para los select de busqueda
+
+marca.addEventListener("change", (e) => {
+
+    datosBusqueda.marca = e.target.value;
+    datosBusqueda.year = e.target.value
+    datosBusqueda.minimo = e.target.value
+    datosBusqueda.maximo = e.target.value
+    datosBusqueda.puertas = e.target.value
+    datosBusqueda.transmision = e.target.value
+    datosBusqueda.color = e.target.value
     
+})
+year.addEventListener("change", (e) => {
+    datosBusqueda.year = e.target.value
+})
+
+minimo.addEventListener("change", (e) => {
+
+    datosBusqueda.minimo = e.target.value
+
+})
+
+maximo.addEventListener("change", (e) => {
+
+    datosBusqueda.maximo = e.target.value
+
+})
+
+puertas.addEventListener("change", (e) => {
+
+    datosBusqueda.puertas = e.target.value
+
+})
+
+transmision.addEventListener("change", (e) => {
+
+    datosBusqueda.transmision = e.target.value
+})
+color.addEventListener("change", (e) => {
+
+    datosBusqueda.color = e.target.value
+    console.log(datosBusqueda)
+})
+
 //Funciones
 
 function mostrarAutos() {
@@ -40,3 +109,6 @@ function llenarSelect() {
         year.appendChild(opcion);  //agrega las opciones de año al select
     }
 }
+
+
+
